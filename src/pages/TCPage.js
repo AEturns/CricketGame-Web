a186aefe81f26431d2b7fead7383fbe6c240b787
hasinthaSnc <139563894+hasinthaSnc@users.noreply.github.com>
2023-09-18 18:14:28 +0530

    save changes

--- src/pages/TCPage.js
+++ src/pages/TCPage.js
@@ -40,6 +40,36 @@ function TCPage() {
         <List className="card-list-faq" style={{ padding: "30px" }}>
           <CCard className="mt-3">
             <CCardBody>
+            <p>Service Provider - Dialog</p>
+              <p>
+                You hereby agree that You have read and fully understood the
+                terms and conditions of the third party mobile network
+                operator/s at https://dlg.dialog.lk/tc/vas. Your use of this
+                website constitutes Your agreement to follow and be bound by
+                such terms and conditions.
+              </p>
+              <p>
+                තුන්වන පාර්ශ්වයේ ජංගම ජාල ක්‍රිකාකරු/ක්‍රියාකරුවන් සඳහා වන,
+                https://dlg.dialog.lk/tc/vas වෙබ් අඩවියේ තිබෙන නියමයන් සහ
+                කොන්දේසි ඔබ විසින් කියවා මැනවින් වටහා ගෙන ඇති බවට ඔබ මෙයින්
+                එකඟවනු ලැබේ. මෙම වෙබ් අඩවිය ඔබ විසින් භාවිත කිරීම හේතුවෙන්ම මෙම
+                නියමයන් සහ කොන්දේසි අනුගමනය කොට ඒවායින් බැඳී සිටින බවට වන ඔබගේ
+                එකඟතාව පිළිබිඹු වන බව සලකන්න.
+              </p>
+              <p>
+                https://dlg.dialog.lk/tc/vas இணையத்தளத்தில் மூன்றாம் தரப்பு
+                கையடக்கத்தொலைபேசி வலைப்பின்னல் இயக்குனர்/களின் நியதிகள் மற்றும்
+                நிபந்தனைகளை நீங்கள் வாசித்து முழுமையாக புரிந்து கொண்டீர்கள்
+                என்பதை இத்தால் ஒப்புக்கொள்கிறீர்கள். இந்த இணையத்தளத்தை நீங்கள்
+                பயன்படுத்துவதானது, அத்தகைய நியதிகள் மற்றும் நிபந்தனைகளைப்
+                பின்பற்றுவதற்கும் மற்றும் அவற்றுக்கு கட்டுப்படுவதற்குமான உங்கள்
+                உடன்பாட்டை உருவாக்குகிறது.
+              </p>
+            </CCardBody>
+          </CCard>
+          <CCard className="mt-3">
+            <CCardBody>
+              <p>Service Provider - Mobitel</p>
               <p>
                 You hereby agree that You have read and fully understood the
                 terms and conditions of the third party mobile network
