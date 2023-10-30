503e0628acb3dd70629d55636d15c37f5ac4bfd3
hasinthaSnc <139563894+hasinthaSnc@users.noreply.github.com>
2023-10-30 12:01:21 +0530

    save chagns

--- src/components/Login.js
+++ src/components/Login.js
@@ -106,11 +106,11 @@ function Login({changeStatus}) {
                     </div>
                 </div>
             </div>
-            <div style={{color: "white", fontSize: "0.8em"}}>
+            {/* <div style={{color: "white", fontSize: "0.8em"}}>
             <span >Rs. 6.00 + taxes daily</span> <br />
             <span >Rs. 180.00 + taxes monthly</span> <br />
             <span >(Auto renewal)</span>
-            </div>
+            </div> */}
             
             {/* <br /> */}
             <div className='animate__animated animate__backInUp '>
